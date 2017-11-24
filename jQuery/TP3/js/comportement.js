77bb4d7edfaf77d8fb92f903bc4f8c77521434c0
Justin North <justinnorth@eduroam17556.univ-lr.fr>
2017-11-24 10:31:06 +0100

    Première version avec 4 appels AJAX

--- jQuery/TP3/js/comportement.js
+++ jQuery/TP3/js/comportement.js
@@ -3,7 +3,37 @@ $(document).ready(init());
 function init(){
     $("#changer").click(
         function(){
-            $.ajax({
+            $.get(
+                "num.php",
+                function traiterAjax(data){
+                    var numero = data;
+                    $.get(
+                        "nom.php",{
+                            n: numero
+                        },
+                        function affichageNom(data){
+                            $("h1#modele").text(data);
+                        }
+                    );
+                    $.get(
+                        "commentaire.php",{
+                            n: numero
+                        },
+                        function affichageComm(data){
+                            $("p#article").text(data);
+                        }
+                    );
+                    $.get(
+                        "photo.php",{
+                            n: numero
+                        },
+                        function affichagePhoto(data){
+                            $("img#imgPhone").attr('src', 'Photos/'+data);
+                        }
+                    );
+                }
+            );
+            /*$.ajax({
                 url: "traitement.php",
                 type: "GET",
                 error: function(){
@@ -12,7 +42,7 @@ function init(){
                 success: function(data){
                     $("#article").text(data);
                 }
-            });
+            });*/
         }
     );
 }
