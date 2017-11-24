d44a7096731f57e5fea9777b94bc25d80f83b039
Justin North <justinnorth@eduroam17556.univ-lr.fr>
2017-11-24 12:36:02 +0100

    Création du dossier 'Exercice 2' et du fichier suavegarde.php

---
+++ jQuery/TP3/Exercice 2/sauvegarde.php
@@ -0,0 +1,17 @@
+<?php
+
+?>
+
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Exercice 2 - TP3</title>
+</head>
+<body>
+    <h1>Sauvegarde automatique</h1>
+    <form name="ajax">
+        <p><input type="text" name="nom"></p>
+    </form>
+</body>
+</html>
