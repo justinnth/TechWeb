c9f59aedf8f328f36ef584969e078cc73b96b695
Justin North <justinnorth@eduroam17556.univ-lr.fr>
2017-11-24 12:39:18 +0100

    Exercice 2 TP3

--- jQuery/TP3/Exercice 2/sauvegarde.php
+++ jQuery/TP3/Exercice 2/sauvegarde.php
@@ -11,7 +11,10 @@
 <body>
     <h1>Sauvegarde automatique</h1>
     <form name="ajax">
-        <p><input type="text" name="nom"></p>
+        <p><input type="text" name="nom" value="Entrez votre nom"></p>
     </form>
+
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+    <script src="js/comportement.js"></script>
 </body>
 </html>
