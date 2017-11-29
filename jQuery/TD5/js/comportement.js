78e45e0f2e36a264d020b54e1ba357e0e2ad9aac
Justin North <justinnorth@eduroam17556.univ-lr.fr>
2017-11-29 09:23:06 +0100

    Update TD5 jQuery terminé

--- jQuery/TD5/js/comportement.js
+++ jQuery/TD5/js/comportement.js
@@ -1,9 +1,21 @@
+var map;
+
 $(document).ready(init());
 
 function init(){
-    envoiAjax("http://odata.bordeaux.fr/v1/databordeaux/parcjardin/?format=json", "bordeaux");
+    //Création de carte de Bordeaux
+    map = new GMaps({
+        div: '#carte',
+        lat: 44.8367819,
+        lng: -0.5762712,
+        zoom: 12
+    });
 
-    envoiAjax("https://api.flickr.com/services/feeds/photos_public.gne? tags=larochelle&tagmode=any&format=json&jsoncallback=?", "image");
+    var urlBordeaux = "http://odata.bordeaux.fr/v1/databordeaux/parcjardin/?format=json"
+    envoiAjax(urlBordeaux, "bordeaux").done(traitemenJardin);
+
+    var urlFlickr = "https://api.flickr.com/services/feeds/photos_public.gne? tags=larochelle&tagmode=any&format=json&jsoncallback=?"
+    envoiAjax(urlFlickr, "image").done(traitemenFlickr);
 }
 
 function envoiAjax(url, classe){
@@ -11,55 +23,46 @@ function envoiAjax(url, classe){
         url: url,
         type: "GET",
         dataType: "JSONP",
-        beforeSend: showLoadingImgFn
+        beforeSend: showLoadingImgFn(classe)
     }).always(function(){
         //Suppression du gif animé
+        $("div."+classe+" img").remove();
     }).fail(function(){
-        alert("Fail");
-    }).done(function(data){
-        switch(classe){
-            case "bordeaux":
-                var ul = $("h2."+classe).next();
-            
-                var map = new GMaps({
-                    div: '#carte',
-                    lat: 44.8367819,
-                    lng: -0.5762712,
-                    zoom: 12
-                });
-            
-                $.each(data.d, function(i){
-                    var li = $("<li/>").attr("data-id", data.d[i].cle).text(data.d[i].nom_espace_entretien).appendTo(ul);
-                    map.addMarker({
-                        lat: data.d[i].y_lat,
-                        lng: data.d[i].x_long,
-                        title: data.d[i].nom_espace_entretien,
-                        mouseover: function(e){
-                            $("ul").find('[data-id='+data.d[i].cle+"]").toggleClass('rouge');
-                        },
-                        mouseout: function(e){
-                            $("ul").find('[data-id='+data.d[i].cle+"]").toggleClass('rouge');
-                        }
-                    });
-                });
-                break;
-            case 'image' :
-                var div = $("div."+classe);
-
-                $.each(data.items, function(i){
-                    var img = $("<img/>").attr('src', data.items[i].media.m).appendTo(div);
-                });
-
-                div.slick({
-                    dots: true,
-                    infinite: true,
-                    fade: true,
-                    speed: 500,
-                    cssEase: 'linear'
-                });
-                break;
-        }
+        alert("Echec");
     });
 }
 
-function showLoadingImgFn(){}
+function traitemenJardin(data){
+    var ul = $("div.bordeaux ul");
+
+    $.each(data.d, function(i){
+        var li = $("<li/>").attr("data-id", data.d[i].cle).text(data.d[i].nom_espace_entretien).appendTo(ul);
+        map.addMarker({
+            lat: data.d[i].y_lat,
+            lng: data.d[i].x_long,
+            title: data.d[i].nom_espace_entretien,
+            mouseover: function(e){
+                $("ul").find('[data-id='+data.d[i].cle+"]").toggleClass('rouge');
+            },
+            mouseout: function(e){
+                $("ul").find('[data-id='+data.d[i].cle+"]").toggleClass('rouge');
+            }
+        });
+    });
+}
+
+function traitemenFlickr(data){
+    var div = $("div.image");
+    
+    $.each(data.items, function(i){
+        var img = $("<img/>").attr('src', data.items[i].media.m).appendTo(div);
+    });
+
+    $(".owl-carousel").owlCarousel();
+}
+
+function showLoadingImgFn(param){
+    var div = $("div."+param);
+
+    var img = $("<img/>").attr('src', 'img/ajax-loader.gif').appendTo(div);
+}
