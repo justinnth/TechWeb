71db33738d9da184880a11edbdc216446718c091
Justin North <justinnorth@eduroam17556.univ-lr.fr>
2017-12-01 09:49:24 +0100

    Update TP4 jQuery

--- jQuery/TP4/js/comportement.js
+++ jQuery/TP4/js/comportement.js
@@ -1,37 +1,47 @@
 $(document).ready(init());
 
 function init(){
-    envoiAjax("https://opendata.paris.fr/api/records/1.0/search/?dataset=liste-des-cafes-a-un-euro&facet=arrondissement");
+    var urlParis = "https://opendata.paris.fr/api/records/1.0/search/?dataset=liste-des-cafes-a-un-euro&facet=arrondissement";
+    envoiAjax(urlParis, "listeCafes").done(traitementParis);
 }
 
-function envoiAjax(url){
+function envoiAjax(url, id){
     return $.ajax({
         url: url,
         type: "GET",
-        dataType: "JSON",
-        beforeSend: showLoadingImgFn
+        dataType: "JSONP",
+        beforeSend: showLoadingImgFn(id)
+    }).always(function(){
+        //Suppression du gif animé
+        $("#"+id+" img").remove();
     }).fail(function(){
-        alert('Fail');
-    }).done(function(data){
-        var main = $("main#listeCafes");
-        var aside = $("aside#detailsCafe");
-        var section = $("section#autresCafes");
-
-        var ul = $("<ul/>").appendTo(main);
-
-        $.each(data.records, function(i){
-            var li = $("<li/>").attr('data-arrondissement',data.records[i].fields.arrondissement).text(data.records[i].fields.nom_du_cafe).appendTo(ul);
-            li.mouseover(function(){
-                var arrondissement = $("<p/>").text("Arrondissement : "+data.records[i].fields.arrondissement).appendTo(aside);
-                var nom = $("<p/>").text("Nom : "+data.records[i].fields.nom_du_cafe).appendTo(aside);
-                var adresse = $("<p/>").text("Adresse : "+data.records[i].fields.adresse).appendTo(aside);
-                var prix = $("<p/>").text("Prix : "+data.records[i].fields.prix_comptoir +"€").appendTo(aside);
-            });
-            li.mouseout(function(){
-                aside.find("p").remove();
-            });
-        })
+        alert("Echec");
     });
 }
 
-function showLoadingImgFn(){}
+function traitementParis(data){
+    var main = $("main#listeCafes");
+    var aside = $("aside#detailsCafe");
+    var section = $("section#autresCafes");
+
+    var ul = $("<ul/>").appendTo(main);
+
+    $.each(data.records, function(i,e){
+        var li = $("<li/>").attr('data-arrondissement',e.fields.arrondissement).text(e.fields.nom_du_cafe).appendTo(ul);
+        li.mouseover(function(){
+            var arrondissement = $("<p/>").text("Arrondissement : "+e.fields.arrondissement).appendTo(aside);
+            var nom = $("<p/>").text("Nom : "+e.fields.nom_du_cafe).appendTo(aside);
+            var adresse = $("<p/>").text("Adresse : "+e.fields.adresse).appendTo(aside);
+            var prix = $("<p/>").text("Prix : "+e.fields.prix_comptoir +"€").appendTo(aside);
+        });
+        li.mouseout(function(){
+            aside.find("p").remove();
+        });
+    });
+}
+
+function showLoadingImgFn(id){
+    var div = $("#"+id);
+    
+    var img = $("<img/>").attr('src', 'img/ajax-loader.gif').appendTo(div);
+}
