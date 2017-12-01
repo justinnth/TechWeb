dd6a8d0035d6c1686662bf0d6ca7beba4d92ab7b
Justin North <justinnorth@eduroam17556.univ-lr.fr>
2017-12-01 10:48:51 +0100

    Fin exo1 TP4

--- jQuery/TP4/js/comportement.js
+++ jQuery/TP4/js/comportement.js
@@ -1,8 +1,10 @@
 $(document).ready(init());
 
 function init(){
-    var urlParis = "https://opendata.paris.fr/api/records/1.0/search/?dataset=liste-des-cafes-a-un-euro&facet=arrondissement";
+    var urlParis = "https://opendata.paris.fr/api/records/1.0/search/?dataset=liste-des-cafes-a-un-euro&rows=159&facet=arrondissement";
     envoiAjax(urlParis, "listeCafes").done(traitementParis);
+
+
 }
 
 function envoiAjax(url, id){
@@ -21,27 +23,57 @@ function envoiAjax(url, id){
 
 function traitementParis(data){
     var main = $("main#listeCafes");
-    var aside = $("aside#detailsCafe");
-    var section = $("section#autresCafes");
 
     var ul = $("<ul/>").appendTo(main);
 
     $.each(data.records, function(i,e){
         var li = $("<li/>").attr('data-arrondissement',e.fields.arrondissement).text(e.fields.nom_du_cafe).appendTo(ul);
-        li.mouseover(function(){
-            var arrondissement = $("<p/>").text("Arrondissement : "+e.fields.arrondissement).appendTo(aside);
-            var nom = $("<p/>").text("Nom : "+e.fields.nom_du_cafe).appendTo(aside);
-            var adresse = $("<p/>").text("Adresse : "+e.fields.adresse).appendTo(aside);
-            var prix = $("<p/>").text("Prix : "+e.fields.prix_comptoir +"€").appendTo(aside);
-        });
-        li.mouseout(function(){
-            aside.find("p").remove();
-        });
+        li.click(clickLi);
     });
 }
 
 function showLoadingImgFn(id){
-    var div = $("#"+id);
-    
+    var div = $("#"+id);   
     var img = $("<img/>").attr('src', 'img/ajax-loader.gif').appendTo(div);
+}
+
+function clickLi(){
+    var nomCafe = $(this).text();
+    nomCafe = encodeURIComponent(nomCafe);
+
+    var url = "https://opendata.paris.fr/api/records/1.0/search/?dataset=liste-des-cafes-a-un-euro&facet=arrondissement&refine.nom_du_cafe="+nomCafe;
+    envoiAjax(url, "detailsCafe").done(traitementClickLi);
+
+    var arrondissement = $(this).attr("data-arrondissement");
+
+    var url = "https://opendata.paris.fr/api/records/1.0/search/?dataset=liste-des-cafes-a-un-euro&rows=159&facet=arrondissement&refine.arrondissement="+arrondissement;
+    envoiAjax(url, "autresCafes").done(traitementAutres);
+}
+
+function traitementClickLi(data){
+    var aside = $("aside#detailsCafe");
+
+    aside.find("p").remove();
+
+    var arrondissement = $("<p/>").text("Arrondissement : "+data.records[0].fields.arrondissement).appendTo(aside);
+    var nom = $("<p/>").text("Nom : "+data.records[0].fields.nom_du_cafe).appendTo(aside);
+    var adresse = $("<p/>").text("Adresse : "+data.records[0].fields.adresse).appendTo(aside);
+    var prix = $("<p/>").text("Prix : "+data.records[0].fields.prix_comptoir +"€").appendTo(aside);
+}
+
+function traitementAutres(data){
+    var section = $("section#autresCafes");
+
+    section.find("p").remove();
+    section.find("h4").remove();
+    section.find("ul").remove();
+
+    var arrondissement = $("<p/>").text("Arrondissement : "+data.parameters.refine.arrondissement).appendTo(section);
+    var nbCafes = $("<p/>").text("Nombre de cafés : "+data.nhits).appendTo(section);
+
+    var h2 = $("<h4/>").text("Liste des cafés de cet arrondissement :").appendTo(section);
+    var ul = $("<ul/>").appendTo(section);
+    $.each(data.records, function(i,e){
+        var li = $("<li/>").attr('data-arrondissement',e.fields.arrondissement).text(e.fields.nom_du_cafe).appendTo(ul);
+    });
 }
