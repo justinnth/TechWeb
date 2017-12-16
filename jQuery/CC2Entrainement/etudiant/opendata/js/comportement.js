b86ea2b4505977bf8dc7fbe09abeadee91fbcae8
Justin North <justin_n@hotmail.fr>
2017-12-16 13:12:39 +0100

    Début partie 2

--- jQuery/CC2Entrainement/etudiant/opendata/js/comportement.js
+++ jQuery/CC2Entrainement/etudiant/opendata/js/comportement.js
@@ -1,7 +1,9 @@
+var map;
+
 $(document).ready(init());
 
 function init(){
-  var map = L.map('map').setView([51.505, -0.09], 13);
+  map = L.map('map').setView([48.856, 2.352], 12);
   L.tileLayer(
     'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
     {attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'}
@@ -19,6 +21,16 @@ function envoiAjax(url){
   });
 }
 
-function traitementArbre(data){
-  
+function traitementArbres(data){
+  var nbArbres = data.nhits;
+  console.log(nbArbres);
+  var strong = $("p.nbArbres strong");
+  strong.text(nbArbres);
+  console.log("Espece : " +data.records[0].fields.espece);
+  console.log("Hauteur : " +data.records[0].fields.hauteurenm + " mètres");
+  $.each(data.records, function(key, value) {
+    if (value.fields.hauteurenm < 25) {
+      L.marker(value.fields.geom_x_y).addTo(map).bindPopup('<h3>'+ value.fields.espece +'</h3><p>'+ value.fields.genre +'</p><p>'+ value.fields.adresse +'</p>')
+    }
+  });
 }
