46613f98665b0877632e8df1a73aae953b0a5cd4
Justin North <justin_n@hotmail.fr>
2017-12-16 12:49:28 +0100

    Ré indentation 2e partie

--- jQuery/CC2Entrainement/etudiant/opendata/index.php
+++ jQuery/CC2Entrainement/etudiant/opendata/index.php
@@ -1,47 +1,31 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<title>openData</title>
-	   <meta charset="utf-8">
-	   
-	    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.0.2/dist/leaflet.css" />
-	    <script src="https://unpkg.com/leaflet@1.0.2/dist/leaflet.js"></script>
+<head>
+	<title>openData</title>
+	<meta charset="utf-8">
 
-	
-   
-   
-   <style>
-        
-    #map{
-        width:800px;
-        height:600px;
-    }
-        
-       body{
-           background-color: #111;
-           color:white;
-       }
-    </style>
-   
-    
-    </head>
+	<link rel="stylesheet" href="https://unpkg.com/leaflet@1.0.2/dist/leaflet.css" />
+	<script src="https://unpkg.com/leaflet@1.0.2/dist/leaflet.js"></script>
+	<style>
+	#map{
+		width:800px;
+		height:600px;
+	}
+
+	body{
+		background-color: #111;
+		color:white;
+	}
+	</style>
+</head>
+
 <body>
-    
-       
-    <h2>Les arbres remarquables de la ville de Paris</h2>
-      
-    <p class="nbArbres">Nombre d'arbres' : <strong></strong></p>
-      
-    <section id="map"></section>
-   
- 
-          
+	<h2>Les arbres remarquables de la ville de Paris</h2>
 
-          
-    
-    
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.js" type="text/javascript"></script>
-   
-   <script src="js/comportement.js" type="text/javascript"></script>
+	<p class="nbArbres">Nombre d'arbres : <strong></strong></p>
+	<section id="map"></section>
+
+	<script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+	<script src="js/comportement.js" type="text/javascript"></script>
 </body>
 </html>
--- jQuery/CC2Entrainement/etudiant/opendata/js/comportement.js
+++ jQuery/CC2Entrainement/etudiant/opendata/js/comportement.js
@@ -1,38 +1,24 @@
-$(document).ready(init);
+$(document).ready(init());
 
 function init(){
-    
-    var map = L.map('map').setView([51.505, -0.09], 13);
+  var map = L.map('map').setView([51.505, -0.09], 13);
+  L.tileLayer(
+    'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
+    {attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'}
+  ).addTo(map);
 
-L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
-    
-    //on prend les données de l'open data
-     var url = "https://opendata.paris.fr/api/records/1.0/search/?dataset=arbresremarquablesparis2011&facet=genre&facet=espece&rows=201";
-    
-      envoiAjax(url).done(traitementArbres);
-    
-    
-    
+  //on prend les données de l'open data
+  var url = "https://opendata.paris.fr/api/records/1.0/search/?dataset=arbresremarquablesparis2011&facet=genre&facet=espece&rows=201";
+  envoiAjax(url).done(traitementArbres);
 }
 
-function envoiAjax(url)
-      {
-          
-        return $.ajax({
-            url: url,
-            dataType: 'jsonp',
-
-      });
-
+function envoiAjax(url){
+  return $.ajax({
+    url: url,
+    dataType: 'jsonp',
+  });
 }
 
 function traitementArbre(data){
-
-
-}
-    
-    
-    
+  
 }
