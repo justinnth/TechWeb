e15bb5322e19efbde5503e1dfb2afa242c2001ec
Justin North <justin_n@hotmail.fr>
2017-12-22 11:05:38 +0100

    Partie 3 - Deuxième point

--- jQuery/CC2jQueryNorth/opendata/js/comportement.js
+++ jQuery/CC2jQueryNorth/opendata/js/comportement.js
@@ -1,5 +1,7 @@
 $(document).ready(init());
 
+var map;
+
 function init() {
   var url = "http://api.bandsintown.com/events/search?api_version=2.0&app_id=mon_app&location=la%20rochelle,france&radius=10&format=json";
   envoiAjax(url).done(traitementAjax);
@@ -16,18 +18,34 @@ function traitementAjax(data) {
 function traitementAjax2(data) {
   var ul = $("ul.concert");
 
+  map = new GMaps({
+    div: '#map',
+    lat: 46.160329,
+    lng: -1.1511390000000574,
+    zoom: 12
+  });
+
   $.each(data, function (key, value) {
     var li = $("<li/>").appendTo(ul);
 
     var h2 = $("<h2/>").text(value.venue.name).appendTo(li);
     var date = new Date(value.datetime);
     var dateString = date.getUTCDate() + "/" + (date.getUTCMonth()+1) + "/" + date.getUTCFullYear() + " à " + date.getUTCHours() + "h" + date.getUTCMinutes();
-    
+
     var p = $("<p/>").text(dateString).appendTo(li);
     var ol = $("<ol/>").appendTo(li);
     $.each(value.artists, function (key, value) {
       li = $("<li/>").text(value.name).appendTo(ol);
-    })
+    });
+
+    map.addMarker({
+      lat: value.venue.latitude,
+      lng: value.venue.longitude,
+      title: value.venue.name,
+      infoWindow: {
+        content: "<a href=\"" + value.venue.url + "\">"+ value.venue.url +"</a>"
+      }
+    });
   })
 }
 
