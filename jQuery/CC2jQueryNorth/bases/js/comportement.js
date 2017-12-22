4ed27f6c4f38ec02a14bea5dfb163958923b6179
Justin North <justin_n@hotmail.fr>
2017-12-22 09:47:03 +0100

    Partie 1 - Exercice 2

--- jQuery/CC2jQueryNorth/bases/js/comportement.js
+++ jQuery/CC2jQueryNorth/bases/js/comportement.js
@@ -2,6 +2,7 @@ $(document).ready(init());
 
 function init() {
   exo1();
+  exo2();
 }
 
 /**
@@ -11,4 +12,27 @@ function exo1() {
   var liste = $("section#ex1 ul");
   var li = $("<li/>").text("jQuery").appendTo(liste);
   li.css('background-color', 'grey');
+}
+
+/**
+ * Exercice 2
+ */
+function exo2() {
+  $("button.valide").click(actionClick);
+}
+
+function actionClick() {
+  var listeSportifs = $("ul.sportif li");
+  var listeGolf = $("ul.listeGolf");
+  var listeAutre = $("ul.listeAutre");
+
+  listeSportifs.each(function(){
+    var li;
+    if ($(this).attr('class') == 'golf') {
+      li = $("<li/>").text($(this).text()).addClass($(this).attr('class')).appendTo(listeGolf);
+    }
+    else {
+      li = $("<li/>").text($(this).text()).addClass($(this).attr('class')).appendTo(listeAutre);
+    }
+  })
 }
